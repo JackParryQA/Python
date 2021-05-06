0577b60f80c325b6b29d3d0d498586076fd6a61d
Jack Parry <JParry@qa.com>
2021-05-06 15:49:26 +0100

    OOP exercises

--- OOP/student_exercise.py
+++ OOP/student_exercise.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self,name,age,s_class):
+    def __init__(self,name,age,s_class="student"):
         self.name=name
         self.age=age
         self.s_class=s_class
@@ -8,6 +8,8 @@ class Student:
         return (test1+test2+test3)/3
 
 
-Jack=Student("Jack", 23, "Computer Science")
+# Jack=Student("Jack", 23, "Computer Science")
+Jack=Student("Jack", 23)
 
 print(Jack.avg_test_score(70,60,65))
+print(getattr(Jack, "s_class"))
